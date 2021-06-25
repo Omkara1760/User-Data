db7ead2a02fd5668c24dc53813a726fb6dec946c
Omkar Varhadi <72789822+Omkara1760@users.noreply.github.com>
2021-06-25 20:11:47 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,7 @@ import firebase from "firebase/app";
 import 'firebase/database';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyCY14K0_eZYsM02LGU0hJXFNtdoRwQ75Ng",
-  authDomain: "user-details-e8fee.firebaseapp.com",
-  projectId: "user-details-e8fee",
-  storageBucket: "user-details-e8fee.appspot.com",
-  messagingSenderId: "504625431993",
-  appId: "1:504625431993:web:afebc220774686cc62a268",
+      // add your firebase configuration
 };
 
 firebase.initializeApp(firebaseConfig);
